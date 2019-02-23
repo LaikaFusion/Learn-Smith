89fd092dac9a93d553869ef98464e8a5af55e911
LaikaFusion <newrad4@gmail.com>
2019-02-23 18:16:52 -0500

    added to root reducer

--- learn-smith-frontend/src/redux/rootReducers.js
+++ learn-smith-frontend/src/redux/rootReducers.js
@@ -1,10 +1,26 @@
 import { combineReducers } from "redux-starter-kit";
 import { counterReducer } from "./counterreducer";
 import { incrimenterReducer } from "./incrimentorreducer";
+import { woodReducer } from "./woodreducer";
+import { ironReducer } from "./ironreducer";
+import { steelReducer } from "./steelreducer";
+import { silverReducer } from "./silverreducer";
+import { copperReducer } from "./copperreducer";
+import { bronzeReducer } from "./bronzereducer";
+import { obsidianReducer } from "./obsidianreducer";
+import { goldReducer } from "./goldreducer";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
-  incrimenter: incrimenterReducer
+  incrimenter: incrimenterReducer,
+  wood: woodReducer,
+  iron: ironReducer,
+  steel: steelReducer,
+  silver: silverReducer,
+  copper: copperReducer,
+  bronze: bronzeReducer,
+  obsidian: obsidianReducer,
+  gold: goldReducer
 });
 
 export default rootReducer;
