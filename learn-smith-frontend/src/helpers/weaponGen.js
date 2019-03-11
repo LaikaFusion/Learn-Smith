de5f0322c9277e8dc3c1cf932696e22ac4ea4282
LaikaFusion <newrad4@gmail.com>
2019-03-11 15:58:48 -0400

    initial recipes

---
+++ learn-smith-frontend/src/helpers/weaponGen.js
@@ -0,0 +1,109 @@
+const recipes = {
+  "Longsword":{
+  },
+  "Shortsword":{
+
+  },
+  "Battle axe":{
+
+  },
+  "Mace":{
+
+  },
+  "Maul":{
+
+  },
+  "Morning star":{
+
+  },
+  "Flail":{
+
+  },
+  "Dagger":{
+
+  },
+  "War hammer":{
+
+  },
+  "Lance":{
+
+  },
+  "Spear":{
+
+  },
+  "Longbow":{
+
+  },
+  "Crossbow":{
+
+  },
+  "Chakram":{
+
+  },
+  "Ulfberht":{
+
+  },
+  "Staff":{
+
+  },
+  "Halberd":{
+
+  },
+  "Club":{
+
+  },
+  "Katana":{
+
+  },
+  "Mail":{
+
+  },
+  "Plate Armor":{
+
+  },
+  "Round Shield":{
+
+  },
+  "Handaxe":{
+
+  },
+  "Sickle":{
+
+  },
+  "Sling":{
+
+  },
+  "Rapier":{
+
+  },
+  "Trident":{
+
+  },
+  "Nailbat":{
+
+  },
+  "Horseshoe on a String":{
+
+  },
+  "Macuahuitl":{
+
+  },
+  "Tower Shield":{
+
+  },
+  "Helmet":{
+
+  },
+  "Boots":{
+
+  },
+  "Gauntlets":{
+
+  },
+  "Bucket Helm":{
+
+  },
+  "Flamberge":{
+
+  }
+}
