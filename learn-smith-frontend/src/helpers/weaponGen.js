3c362a87dc6941714608c079b0cbb6a28efe6305
LaikaFusion <newrad4@gmail.com>
2019-03-11 16:36:49 -0400

    added recipe generator

--- learn-smith-frontend/src/helpers/weaponGen.js
+++ learn-smith-frontend/src/helpers/weaponGen.js
@@ -1,137 +1,69 @@
+const ingridents = ["wood", "copper", "iron", "steel", "obsidian", "silver"];
 const recipes = {
-  "Longsword":{
-    recipes:["steel","steel"]
+  longsword: {
+    name: "Longsword",
+    recipes: ["steel", "steel"]
   },
-  "Shortsword":{
-    recipes:["steel","silver"]
+  shortsword: { name: "Shortsword", recipes: ["steel", "silver"] },
+  battleaxe: { name: "Battle Axe", recipes: ["steel", "wood"] },
+  mace: { name: "Mace", recipes: ["steel", "copper"] },
+  maul: { name: "Maul", recipes: ["steel", "obsidian"] },
+  morningstar: {
+    name: "Morning Star",
+    recipes: ["steel", "iron"]
   },
-  "Battle axe":{
-    recipes:["steel","wood"]
-  },
-  "Mace":{
-    recipes:["steel","copper"]
-  },
-  "Maul":{
-    recipes:["steel","obsidian"]
-  },
-  "Morning star":{
-    recipes:["steel","iron"]
-  },
-  "Flail":{
-    recipes:["copper","iron"]
-  },
-  "Dagger":{
-    recipes:["copper","silver"]
-  },
-  "War hammer":{
-    recipes:["copper","wood"]
-  },
-  "Lance":{
-    recipes:["copper","copper"]
+  flail: { name: "Flail", recipes: ["copper", "iron"] },
+  dagger: { name: "Dagger", recipes: ["copper", "silver"] },
+  warhammer: { name: "War Hammer", recipes: ["copper", "wood"] },
+  lance: { name: "Lance", recipes: ["copper", "copper"] },
+  spear: { name: "Spear", recipes: ["copper", "obsidian"] },
+  mail: { name: "Mail", recipes: ["copper", "steel"] },
+  longbow: { name: "Longbow", recipes: ["wood", "wood"] },
+  crossbow: { name: "Crossbow", recipes: ["wood", "iron"] },
+  staff: { name: "Staff", recipes: ["wood", "obsidian"] },
+  club: { name: "Club", recipes: ["wood", "silver"] },
+  nailbat: { name: "Nailbat", recipes: ["wood", "steel"] },
+  boots: { name: "Boots", recipes: ["wood", "copper"] },
+  chakram: { name: "Chakram", recipes: ["iron", "iron"] },
+  ulfberht: { name: "Ulfberht", recipes: ["iron", "obsidian"] },
+  halberd: { name: "Halberd", recipes: ["iron", "silver"] },
+  katana: { name: "Katana", recipes: ["iron", "steel"] },
+  platearmor: { name: "Plate Armor", recipes: ["iron", "copper"] },
+  roundshield: { name: "Round Shield", recipes: ["iron", "wood"] },
+  macuahuitl: { name: "Macuahuitl", recipes: ["obsidian", "obsidian"] },
+  handaxe: { name: "Handaxe", recipes: ["obsidian", "steel"] },
+  sickle: { name: "Sickle", recipes: ["obsidian", "wood"] },
+  sling: { name: "Sling", recipes: ["obsidian", "copper"] },
+  rapier: { name: "Rapier", recipes: ["obsidian", "iron"] },
+  trident: { name: "Trident", recipes: ["obsidian", "silver"] },
 
-  },
-  "Spear":{
-    recipes:["copper","obsidian"]
-  },
-  "Mail":{
-    recipes:["copper","steel"]
+  horsestring: { name: "Horseshoe on a String", recipes: ["silver", "wood"] },
 
-  },
-  "Longbow":{
-    recipes:["wood","wood"]
-  },
-  "Crossbow":{
-    recipes:["wood","iron"]
-  },
-  "Staff":{
-    recipes:["wood","obsidian"]
+  towershield: { name: "Tower Shield", recipes: ["silver", "steel"] },
+  helmet: { name: "Helmet", recipes: ["silver", "iron"] },
 
-  },
-  "Club":{
-    recipes:["wood","silver"]
+  gauntlets: { name: "Gauntlets", recipes: ["silver", "copper"] },
+  buckethelm: { name: "Bucket Helm", recipes: ["silver", "obsidian"] },
+  flamberge: { name: "Flamberge", recipes: ["silver", "silver"] }
+};
 
-  },
-  "Nailbat":{
-    recipes:["wood","steel"]
+const randomProperty = function(obj) {
+  const keys = Object.keys(obj);
+  return obj[keys[(keys.length * Math.random()) << 0]];
+};
 
-  },
-  "Boots":{
-    recipes:["wood","copper"]
-
-  },
-  "Chakram":{
-    recipes:["iron","iron"]
-
-  },
-  "Ulfberht":{
-    recipes:["iron","obsidian"]
-
-  },
-  "Halberd":{
-    recipes:["iron","silver"]
-
-  },
-
-  "Katana":{
-    recipes:["iron","steel"]
-
-  },
-
-  "Plate Armor":{
-    recipes:["iron","copper"]
-
-  },
-  "Round Shield":{
-    recipes:["iron","wood"]
-  },
-  "Macuahuitl":{
-    recipes:["obsidian","obsidian"]
-  },
-  "Handaxe":{
-    recipes:["obsidian","steel"]
-
-  },
-  "Sickle":{
-    recipes:["obsidian","wood"]
-
-  },
-  "Sling":{
-    recipes:["obsidian","copper"]
-
-  },
-  "Rapier":{
-    recipes:["obsidian","iron"]
-
-  },
-  "Trident":{
-    recipes:["obsidian","silver"]
-
-  },
-
-  "Horseshoe on a String":{
-    recipes:["silver","wood"]
-
-  },
-  
-  "Tower Shield":{
-    recipes:["silver","steel"]
-
-  },
-  "Helmet":{
-    recipes:["silver","iron"]
-
-  },
-
-  "Gauntlets":{
-    recipes:["silver","copper"]
-
-  },
-  "Bucket Helm":{
-    recipes:["silver","obsidian"]
-
-  },
-  "Flamberge":{
-    recipes:["silver","silver"]
-
-  }
+function jsUcfirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+const weaponRecipe = () => {
+  const weaponObj = randomProperty(recipes);
+  const material = ingridents[Math.floor(Math.random() * ingridents.length)];
+  weaponObj.recipes.unshift(material);
+  weaponObj.name = `${jsUcfirst(material)} ${weaponObj.name}`;
+  weaponObj.goldVal =
+    Math.floor(Math.random() * 10 + 1) + ingridents.indexOf(material) + 1;
+  return weaponObj;
+};
+
+export default weaponRecipe;
