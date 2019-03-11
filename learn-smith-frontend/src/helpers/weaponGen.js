6d40f3b27217b3c2645b770683e1c7e7d88712ef
LaikaFusion <newrad4@gmail.com>
2019-03-11 16:13:03 -0400

    added recipes

--- learn-smith-frontend/src/helpers/weaponGen.js
+++ learn-smith-frontend/src/helpers/weaponGen.js
@@ -1,109 +1,137 @@
 const recipes = {
   "Longsword":{
+    recipes:["steel","steel"]
   },
   "Shortsword":{
-
+    recipes:["steel","silver"]
   },
   "Battle axe":{
-
+    recipes:["steel","wood"]
   },
   "Mace":{
-
+    recipes:["steel","copper"]
   },
   "Maul":{
-
+    recipes:["steel","obsidian"]
   },
   "Morning star":{
-
+    recipes:["steel","iron"]
   },
   "Flail":{
-
+    recipes:["copper","iron"]
   },
   "Dagger":{
-
+    recipes:["copper","silver"]
   },
   "War hammer":{
-
+    recipes:["copper","wood"]
   },
   "Lance":{
+    recipes:["copper","copper"]
 
   },
   "Spear":{
+    recipes:["copper","obsidian"]
+  },
+  "Mail":{
+    recipes:["copper","steel"]
 
   },
   "Longbow":{
-
+    recipes:["wood","wood"]
   },
   "Crossbow":{
-
-  },
-  "Chakram":{
-
-  },
-  "Ulfberht":{
-
+    recipes:["wood","iron"]
   },
   "Staff":{
-
-  },
-  "Halberd":{
+    recipes:["wood","obsidian"]
 
   },
   "Club":{
-
-  },
-  "Katana":{
-
-  },
-  "Mail":{
-
-  },
-  "Plate Armor":{
-
-  },
-  "Round Shield":{
-
-  },
-  "Handaxe":{
-
-  },
-  "Sickle":{
-
-  },
-  "Sling":{
-
-  },
-  "Rapier":{
-
-  },
-  "Trident":{
+    recipes:["wood","silver"]
 
   },
   "Nailbat":{
-
-  },
-  "Horseshoe on a String":{
-
-  },
-  "Macuahuitl":{
-
-  },
-  "Tower Shield":{
-
-  },
-  "Helmet":{
+    recipes:["wood","steel"]
 
   },
   "Boots":{
+    recipes:["wood","copper"]
 
   },
+  "Chakram":{
+    recipes:["iron","iron"]
+
+  },
+  "Ulfberht":{
+    recipes:["iron","obsidian"]
+
+  },
+  "Halberd":{
+    recipes:["iron","silver"]
+
+  },
+
+  "Katana":{
+    recipes:["iron","steel"]
+
+  },
+
+  "Plate Armor":{
+    recipes:["iron","copper"]
+
+  },
+  "Round Shield":{
+    recipes:["iron","wood"]
+  },
+  "Macuahuitl":{
+    recipes:["obsidian","obsidian"]
+  },
+  "Handaxe":{
+    recipes:["obsidian","steel"]
+
+  },
+  "Sickle":{
+    recipes:["obsidian","wood"]
+
+  },
+  "Sling":{
+    recipes:["obsidian","copper"]
+
+  },
+  "Rapier":{
+    recipes:["obsidian","iron"]
+
+  },
+  "Trident":{
+    recipes:["obsidian","silver"]
+
+  },
+
+  "Horseshoe on a String":{
+    recipes:["silver","wood"]
+
+  },
+  
+  "Tower Shield":{
+    recipes:["silver","steel"]
+
+  },
+  "Helmet":{
+    recipes:["silver","iron"]
+
+  },
+
   "Gauntlets":{
+    recipes:["silver","copper"]
 
   },
   "Bucket Helm":{
+    recipes:["silver","obsidian"]
 
   },
   "Flamberge":{
+    recipes:["silver","silver"]
 
   }
 }
